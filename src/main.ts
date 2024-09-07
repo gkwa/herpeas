d4802370e53d22eea0e628dd89ab771ff6c83b6a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-09-06 21:33:44 -0400

    first phase ok

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
-import { PlaywrightCrawler, LogLevel, log } from "crawlee";
+import { CheerioCrawler, LogLevel, log } from "crawlee";
 
 const startUrl = process.env.START_URL || "https://crawlee.dev";
 
-const crawler = new PlaywrightCrawler({
-  async requestHandler({ request, page, enqueueLinks, log, pushData }) {
-    const title = await page.title();
+const crawler = new CheerioCrawler({
+  async requestHandler({ $, request, enqueueLinks, log, pushData }) {
+    const title = $("title").text();
     log.info(`Title of ${request.loadedUrl} is '${title}'`);
     await pushData({ title, url: request.loadedUrl });
-
     await enqueueLinks({
-      strategy: "same-domain",
+      selector: 'a[href*="go.php?ID="]',
       transformRequestFunction: (req) => {
         log.info(`Enqueueing: ${req.url}`);
         return req;
       },
     });
   },
-  maxRequestsPerCrawl: 20,
-  headless: false, // Set to false to show the browser
+  maxRequestsPerCrawl: 5,
 });
 
 log.setLevel(LogLevel.INFO);
-
 await crawler.run([startUrl]);
